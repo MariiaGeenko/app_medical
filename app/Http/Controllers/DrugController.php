d853b4254693fee7b77f5bd3cdd5ff3fcd50c53b
Mariia <88836436+MariiaGeenko@users.noreply.github.com>
2023-03-18 20:19:48 +0100

    added controllers and Enum

--- app/Http/Controllers/DrugController.php
+++ app/Http/Controllers/DrugController.php
@@ -3,10 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\View\View;
 
 class DrugController extends Controller
 {
-    public function index()
+    /**
+     * @return View
+     */
+    public function index(): View
     {
         return (\view('drugs.index'));
     }
