70ea2c9765ded0cec5f01f63b3280c518bcc7816
avbmoons <88578581+avbmoons@users.noreply.github.com>
2023-04-08 23:10:31 +0300

    routes web and api

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,26 @@
 <?php
 
+declare(strict_types=1);
+
+// это рабочие юзы до установки бриз
+use App\Http\Controllers\Api\DoctorApiController;
+use App\Http\Controllers\DoctorSpecialityController;
+use App\Http\Controllers\PharmaciesHasDrugsController;
+use App\Http\Resources\DrugCollection;
+use App\Models\Drug;
+// use Illuminate\Http\Request;
+// use Illuminate\Support\Facades\Route;
+use \App\Http\Controllers\DrugController;
+use \App\Http\Resources\PharmacyCollection;
+use \App\Models\Subjects\Pharmacy;
+use \App\Http\Resources\DoctorsCollection;
+use \App\Models\Subjects\Doctor;
+use \App\QueryBuilders\DrugsQueryBuilder;
+use \App\Http\Controllers\Api\PharmacyApiController;
+use \App\Http\Controllers\Api\PatientApiController;
+
+
+// при установке бриз были только эти юзы
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +35,47 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// при установке бриз был только этот роут
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// это рабочие роуты до установки бриз
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::get('/drugs', function () {
+    return DrugCollection::collection(Drug::all());
+    // return DrugCollection::collection(DrugsQueryBuilder::getAll());
+});
+
+// Route::get('/pharmacies', function () {
+//     return PharmacyCollection::collection(Pharmacy::all());
+// });
+
+// Route::get('/doctors', function () {
+//     return DoctorsCollection::collection(Doctor::all());
+// });
+
+Route::get('/doctors', [DoctorApiController::class, 'index']);
+
+Route::get('/doctors/speciality/{id}', [DoctorApiController::class, 'getDoctorsWithSpeciality']);
+
+Route::get('/doctors/{id}/receipts', [DoctorApiController::class, 'getDoctorWithReceipts']);
+
+Route::get('/doctors/{id}/patients', [DoctorApiController::class, 'getDoctorWithPatientsWithReceipts']);
+
+
+Route::get('/pharmacies', [PharmacyApiController::class, 'index']);
+
+Route::get('/pharmacies/{id}/drugs', [PharmacyApiController::class, 'getDrugsWithPharmacy']);
+
+Route::get('/pharmacies/drugs/{id}', [PharmacyApiController::class, 'getPharmaciesWithDrugs']);
+
+
+Route::get('/patients', [PatientApiController::class, 'index']);
+
+Route::get('/patients/{id}/receipts', [PatientApiController::class, 'getReceiptsWithPatient']);
+
+Route::get('/receipts/{id}/pharmacies', [PatientApiController::class, 'getPharmaciesWithReceipt']);
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,35 @@
 <?php
 
+declare(strict_types=1);
+
+// это рабочие юзы до установки бриз
+use App\Http\Controllers\DBTestController;
+use App\Http\Controllers\MainController;
+use App\Http\Controllers\Admin\IndexController as AdminIndexController;
+use App\Http\Controllers\Admin\MainController as AdminMainController;
+use App\Http\Controllers\Admin\DrugController as AdminDrugController;
+use App\Http\Controllers\Admin\MessageController as AdminMessageController;
+use App\Http\Controllers\Admin\ClinicController as AdminClinicController;
+use App\Http\Controllers\Admin\DoctorController as AdminDoctorController;
+use App\Http\Controllers\Admin\Doctor_reviewController as AdminDoctor_reviewController;
+use App\Http\Controllers\Admin\MeetingController as AdminMeetingController;
+use App\Http\Controllers\Admin\OrganizationController as AdminOrganizationController;
+use App\Http\Controllers\Admin\OrganizationTypesController as AdminOrganizationTypesController;
+use App\Http\Controllers\Admin\PatientController as AdminPatientController;
+use App\Http\Controllers\Admin\PharmacyController as AdminPharmacyController;
+use App\Http\Controllers\Admin\ReceiptController as AdminReceiptController;
+use App\Http\Controllers\Admin\ServiceController as AdminServiceController;
+use App\Http\Controllers\Admin\SpecialityController as AdminSpecialityController;
+use App\Http\Controllers\Admin\Video_callController as AdminVideo_callController;
+
+use App\Http\Controllers\Admin\UserController as AdminUserController;
+use App\Http\Controllers\DrugController;
+use App\Http\Controllers\MessageController;
+
+// при установке бриз был только этот юз
 use Illuminate\Support\Facades\Route;
+
+
 
 /*
 |--------------------------------------------------------------------------
@@ -12,9 +41,43 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+// при установке бриз был только этот роут
 Route::get('/', function () {
     return ['Laravel' => app()->version()];
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
+
+// это рабочие роуты до установки бриз
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', [MainController::class, 'index'])->name('main');
+
+Route::group(['prefix' => 'admin', 'as' => 'admin.'], static function () {
+    Route::get('/', [AdminIndexController::class, 'index'])->name('admin');
+    Route::resource('main', AdminMainController::class);
+    Route::resource('drugs', AdminDrugController::class);
+    Route::resource('messages', AdminMessageController::class);
+    Route::resource('clinics', AdminClinicController::class);
+    Route::resource('doctors', AdminDoctorController::class);
+    Route::resource('doctor_reviews', AdminDoctor_reviewController::class);
+    Route::resource('meetings', AdminMeetingController::class);
+    Route::resource('organizations', AdminOrganizationController::class);
+    Route::resource('organization_types', AdminOrganizationTypesController::class);
+    Route::resource('patients', AdminPatientController::class);
+    Route::resource('pharmacies', AdminPharmacyController::class);
+    Route::resource('receipts', AdminReceiptController::class);
+    Route::resource('services', AdminServiceController::class);
+    Route::resource('specialities', AdminSpecialityController::class);
+    Route::resource('video_calls', AdminVideo_callController::class);
+
+    Route::resource('users', AdminUserController::class);
+});
+
+Route::get('/drugs', [DrugController::class, 'index'])->name('drugs');
+Route::get('/messages', [MessageController::class, 'index'])->name('messages');
+
+Route::get('dbtest', DBTestController::class);
